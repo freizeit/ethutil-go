b4a603a06dbde18b75efe30e3bb0e2450762e868
obscuren <geffobscura@gmail.com>
2014-02-02 20:54:40 +0100

    Wip logger

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package ethutil
 
 import (
 	"log"
+	"os"
 	"os/user"
 	"path"
 )
@@ -15,9 +16,9 @@ const (
 
 // Config struct isn't exposed
 type config struct {
-	Db  Database
-	Log *log.Logger
+	Db Database
 
+	Log      Logger
 	ExecPath string
 	Debug    bool
 	Ver      string
@@ -32,7 +33,63 @@ func ReadConfig() *config {
 		path := path.Join(usr.HomeDir, ".ethereum")
 
 		Config = &config{ExecPath: path, Debug: true, Ver: "0.0.1"}
+		Config.Log = NewLogger(LogFile|LogStd, 0)
 	}
 
 	return Config
+}
+
+type LoggerType byte
+
+const (
+	LogFile = 0x1
+	LogStd  = 0x2
+)
+
+type Logger struct {
+	logSys   []*log.Logger
+	logLevel int
+}
+
+func NewLogger(flag LoggerType, level int) Logger {
+	var loggers []*log.Logger
+
+	flags := log.LstdFlags | log.Lshortfile
+
+	if flag&LogFile > 0 {
+		file, err := os.OpenFile(path.Join(Config.ExecPath, "debug.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+		if err != nil {
+			log.Panic("unable to create file logger", err)
+		}
+
+		log := log.New(file, "[ETH]", flags)
+
+		loggers = append(loggers, log)
+	}
+	if flag&LogStd > 0 {
+		log := log.New(os.Stdout, "[ETH]", flags)
+		loggers = append(loggers, log)
+	}
+
+	return Logger{logSys: loggers, logLevel: level}
+}
+
+func (log Logger) Debugln(v ...interface{}) {
+	if log.logLevel != 0 {
+		return
+	}
+
+	for _, logger := range log.logSys {
+		logger.Println(v...)
+	}
+}
+
+func (log Logger) Debugf(format string, v ...interface{}) {
+	if log.logLevel != 0 {
+		return
+	}
+
+	for _, logger := range log.logSys {
+		logger.Printf(format, v...)
+	}
 }
